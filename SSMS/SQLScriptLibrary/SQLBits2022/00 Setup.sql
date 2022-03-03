098afc04d8b730ae10ba08f667195fbf876b2d09
Martin Croft <martin.croft@sqlserversolutions.co.uk>
2022-03-03 12:41:05 +0000

    Updates

---
+++ SSMS/SQLScriptLibrary/SQLBits2022/00 Setup.sql
@@ -0,0 +1,12 @@
+/*
+
+Open up C:\SQLScriptsLibrary\SSMS\SQLScriptLibrary\Security\01_Security_Audit.sql
+
+browse to https://github.com/martincroft
+
+Open solution and close solution explorer
+
+
+
+
+*/
--- SSMS/SQLScriptLibrary/SQLBits2022/04 Demo-blank.sql
+++ SSMS/SQLScriptLibrary/SQLBits2022/04 Demo-blank.sql
@@ -3,7 +3,7 @@
 --================================
 
 
-
+-- REMINDER BREAK PRODUCTION --
 
 
 
